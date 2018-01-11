1dd7bcdeddce4c0380f60d7b3b58e4745613839f
YC Xu <x13989@126.com>
2018-01-11 14:28:32 +0800

    Some major changes. Reformat code, cleaned up, standardized parameter names, made the code easier to use. Added some basic functions for the time-calculating system that is going to add soon.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <ctime>
 #include "main.h"
 
-int inputElement(llong in[], llong length) {
+//Main Functions Begin
+typedef void (*FuncType)(llong array[], llong n);
+
+void inputElement(llong in[], llong length) {
     for (llong i = 0; i < length; i += 1) {
         cin >> in[i];
     }
@@ -11,7 +14,17 @@ bool isSame(char *a, const string b) {
     return (strcmp(a, b.data() + '\0') == 0);
 }
 
-llong bubbleSort(llong array[], llong n) {
+llong getTime(llong array[], llong n, FuncType function) {
+    clock_t begin, end;
+    begin = clock();
+    function(array, n);
+    end = clock();
+    return end - begin;
+}
+//Main Functions End
+
+//Bubble Sort Begins
+void bubbleSort(llong array[], llong n) {
     for (llong i = 0; i < n - 1; i += 1) {
         for (llong j = 0; j < n - 1 - i; j += 1) {
             if (array[j] > array[j + 1]) {
@@ -20,8 +33,10 @@ llong bubbleSort(llong array[], llong n) {
         }
     }
 }
+//Bubble Sort Ends
 
-llong insertSort(llong array[], llong n) {
+//Insertion Sort Begins
+void insertSort(llong array[], llong n) {
     llong temp, j;
     for (llong i = 1; i < n; i += 1) {
         temp = array[i];
@@ -31,8 +46,10 @@ llong insertSort(llong array[], llong n) {
         array[j] = temp;
     }
 }
+//Insertion Sort Ends
 
-llong selectSort(llong array[], llong n) {
+//Selection Sort Begins
+void selectSort(llong array[], llong n) {
     llong min;
     for (llong i = 0; i < n - 1; i++) {
         min = i;
@@ -46,7 +63,9 @@ llong selectSort(llong array[], llong n) {
         }
     }
 }
+//Selection Sort Ends
 
+//Merge Sort Begins
 void Merge(llong sourceArr[], llong tempArr[], llong startIndex, llong midIndex, llong endIndex) {
     llong i = startIndex;
     llong j = midIndex + 1;
@@ -69,17 +88,24 @@ void Merge(llong sourceArr[], llong tempArr[], llong startIndex, llong midIndex,
     }
 }
 
-llong MergeSort(llong sourceArr[], llong tempArr[], llong startIndex, llong endIndex) {
+void MSort(llong sourceArr[], llong tempArr[], llong startIndex, llong endIndex) {
     llong midIndex;
     if (startIndex < endIndex) {
         midIndex = (startIndex + endIndex) / 2;
-        MergeSort(sourceArr, tempArr, startIndex, midIndex);
-        MergeSort(sourceArr, tempArr, midIndex + 1, endIndex);
+        MSort(sourceArr, tempArr, startIndex, midIndex);
+        MSort(sourceArr, tempArr, midIndex + 1, endIndex);
         Merge(sourceArr, tempArr, startIndex, midIndex, endIndex);
     }
 }
 
-void quickSort(llong a[], llong low, llong high) {
+void mergeSort(llong array[], llong n) {
+    llong b[n];
+    MSort(array, b, 0, n - 1);
+}
+//Merge Sort Ends
+
+//Quick Sort Begins
+void QSort(llong a[], llong low, llong high) {
     if (low >= high) {
         return;
     }
@@ -101,10 +127,16 @@ void quickSort(llong a[], llong low, llong high) {
         a[last] = a[first];
     }
     a[first] = key;
-    quickSort(a, low, first - 1);
-    quickSort(a, first + 1, high);
+    QSort(a, low, first - 1);
+    QSort(a, first + 1, high);
 }
 
+void quickSort(llong array[], llong n) {
+    QSort(array, 0, n - 1);
+}
+//Quick Sort Ends
+
+//Heap Sort Begins
 void HeapAdjust(llong array[], llong i, llong length) {
     llong nChild;
     llong nTemp;
@@ -123,26 +155,26 @@ void HeapAdjust(llong array[], llong i, llong length) {
     }
 }
 
-//堆排序算法
-void heapSort(llong array[], llong length) {
+void heapSort(llong array[], llong n) {
     llong i;
-    for (i = length / 2 - 1; i >= 0; --i) {
-        HeapAdjust(array, i, length);
+    for (i = n / 2 - 1; i >= 0; --i) {
+        HeapAdjust(array, i, n);
     }
-    for (i = length - 1; i > 0; --i) {
+    for (i = n - 1; i > 0; --i) {
         array[i] = array[0] ^ array[i];
         array[0] = array[0] ^ array[i];
         array[i] = array[0] ^ array[i];
         HeapAdjust(array, 0, i);
     }
 }
+//Heap Sort Ends
 
-llong maxBit(llong data[], llong n) //辅助函数，求数据的最大位数
-{
-    llong d = 1; //保存最大的位数
+//Radix Sort Begins
+llong maxBit(llong array[], llong n) {
+    llong d = 1;
     llong p = 10;
     for (llong i = 0; i < n; ++i) {
-        while (data[i] >= p) {
+        while (array[i] >= p) {
             p *= 10;
             ++d;
         }
@@ -150,38 +182,37 @@ llong maxBit(llong data[], llong n) //辅助函数，求数据的最大位数
     return d;
 }
 
-void radixSort(llong data[], llong n) //基数排序
-{
-    llong d = maxBit(data, n);
+void radixSort(llong array[], llong n) {
+    llong d = maxBit(array, n);
     llong tmp[n];
     llong count[10];
     llong i, j, k;
     llong radix = 1;
-    for (i = 1; i <= d; i++) //进行d次排序
-    {
-        for (j = 0; j < 10; j++) {
-            count[j] = 0; //每次分配前清空计数器
+    for (i = 1; i <= d; i += 1) {
+        for (j = 0; j < 10; j += 1) {
+            count[j] = 0;
         }
-        for (j = 0; j < n; j++) {
-            k = (data[j] / radix) % 10; //统计每个桶中的记录数
+        for (j = 0; j < n; j += 1) {
+            k = (array[j] / radix) % 10;
             count[k]++;
         }
-        for (j = 1; j < 10; j++) {
-            count[j] = count[j - 1] + count[j]; //将tmp中的位置依次分配给每个桶
+        for (j = 1; j < 10; j += 1) {
+            count[j] = count[j - 1] + count[j];
         }
-        for (j = n - 1; j >= 0; j--) {
-            k = (data[j] / radix) % 10;
-            tmp[count[k] - 1] = data[j];
+        for (j = n - 1; j >= 0; j -= 1) {
+            k = (array[j] / radix) % 10;
+            tmp[count[k] - 1] = array[j];
             count[k]--;
         }
         for (j = 0; j < n; j++) {
-            data[j] = tmp[j];
+            array[j] = tmp[j];
             radix = radix * 10;
         }
     }
     delete[] count;
     delete[] tmp;
 }
+//Radix Sort Ends
 
 int main(int argc, char *argv[]) {
     llong length;
@@ -224,28 +255,27 @@ int main(int argc, char *argv[]) {
         cout << "8. All" << endl;
         cin >> index;
     }
-    long long b[length];
     switch (index) {
         case 1:
-            bubbleSort(m, length);
+            getTime(m, length, bubbleSort);
             break;
         case 2:
-            selectSort(m, length);
+            getTime(m, length, selectSort);
             break;
         case 3:
-            insertSort(m, length);
+            getTime(m, length, insertSort);
             break;
         case 4:
-            MergeSort(m, b, 0, length - 1);
+            getTime(m, length, mergeSort);
             break;
         case 5:
-            quickSort(m, 0, length - 1);
+            getTime(m, length, quickSort);
             break;
         case 6:
-            heapSort(m, length);
+            getTime(m, length, heapSort);
             break;
         case 7:
-            radixSort(m, length);
+            getTime(m, length, radixSort);
             break;
         default:
             cout << "Incorrect input! " << endl;
